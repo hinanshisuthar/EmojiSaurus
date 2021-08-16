367b5e717dd526f0941e679a607e4dacd8c44cc7
hinanshisuthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-08-17 01:01:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+const emojiDict = {
+  "🍈": "Melon",
+  "🍇": "Grapes",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🥭": "Mango",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍑": "Peach",
+  "🍒": "Cherries",
+  "🍓": "Strawberry",
+  "🥝": "Kiwi Fruit",
+  "🍅": "Tomato"
+};
+
+var emojiList = ["🍉", "🍋", "🍍", "🥝", "🍅", "🍇", "🍐", "🍎", "🍊", "🥭"];
+
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDict[userInput];
+
+    if (meaning === undefined) {
+      meaning = "Sorry! This emoji does not include in our database";
+    }
+    if (meaning === "") {
+      return;
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDict[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="gap"></div>
+      <h1 className="heading"> Emoji Saurus </h1>
+      <h4 className="sub-heading"> -the fruits edition-</h4>
+      <input className="input" onChange={emojiInputHandler}></input>
+      <div className="meaning">
+        <h1> {meaning} </h1>
+      </div>
+      <h3>or select from below</h3>
+      <ul>
+        {emojiList.map((item) => {
+          return (
+            <li
+              onClick={() => emojiClickHandler(item)}
+              key={item}
+              className="emoji-List"
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
