e98ab8cf85bfba52fbf145e29803d50a62525c9f
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-10-21 14:43:21 +0530

    refactor:  added comments

--- src/App.js
+++ src/App.js
@@ -20,16 +20,20 @@ const emojiDict = {
   "🍅": "Tomato"
 };
 
-var emojiList = Object.keys(emojiDict);
+var emojiList = Object.keys(emojiDict); //returns array of keys i.e. array of emojis
 
 export default function App() {
-  const [meaning, setMeaning] = useState("");
+  // useState returns 2 values, hence we use a list for storing them
+  //meaning is the variable here that we want to render
+  //setMeaning is the setter function 
+  //useState is given an initial value to render
+  const [meaning, setMeaning] = useState(""); 
 
   function emojiInputHandler(event) {
-    var userInput = event.target.value;
+    var userInput = event.target.value; //reading user input
     var meaning = emojiDict[userInput];
 
-    if (userInput in emojiDict) {
+    if (userInput in emojiDict) { // validity check
       setMeaning(emojiDict[userInput]);
     } else if (userInput === "") {
       setMeaning("");
@@ -38,12 +42,12 @@ export default function App() {
     }
   }
 
-  function emojiClickHandler(emoji) {
+  function emojiClickHandler(emoji) { // function to render the meaning
     var meaning = emojiDict[emoji];
     setMeaning(meaning);
   }
 
-  return (
+  return ( //xml = html + js
     <div className="App">
       <div className="gap"></div>
       <h1 className="heading"> Emoji Saurus </h1>
@@ -54,12 +58,12 @@ export default function App() {
       </div>
       <h3>or select from below</h3>
       <ul>
-        {emojiList.map((item) => {
+        {emojiList.map((item) => { //map function to iterate over the object
           return (
             <li
-              onClick={() => emojiClickHandler(item)}
-              key={item}
-              className="emoji-List"
+              onClick={() => emojiClickHandler(item)} // onclick event
+              key={item} 
+              className="emoji-List" // class is a reserved keyword in js so we use className in react
             >
               {item}
             </li>
